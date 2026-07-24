2c9bb254658abe53bcfddb07de1da828d0328935
Lounes BOUTELLIS <lounes.boutellis@fgei.ummto.dz>
2026-07-24 16:45:18 +0100

    Add cli_utils.c with menu display function

---
+++ src/cli_utils.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "../include/cli_utils.h"
+
+void afficher_menu_test() {
+    printf("--- Menu de NetStruct-CLI ---\n");
+    printf("Ici, on mettra les options plus tard.\n");
+}
