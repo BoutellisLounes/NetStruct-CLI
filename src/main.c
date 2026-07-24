82e3a3acad822aaf0323bb73911b66069538e11a
Lounes BOUTELLIS <lounes.boutellis@fgei.ummto.dz>
2026-07-24 16:47:42 +0100

    Add main function to call utility functions

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include "../include/cli_utils.h"
+#include "../include/ip_calc.h"
+
+int main() {
+    // Le main ne fait aucun calcul, il se contente d'appeler les autres fichiers !
+    tester_moteur_ip();
+    afficher_menu_test();
+
+    return 0;
+}
