dab0300f3d1e27dcca7a4590bb235f1299904f5b
Lounes BOUTELLIS <lounes.boutellis@fgei.ummto.dz>
2026-08-06 15:03:25 +0100

    update and r

---
+++ include/cli_user.h
@@ -0,0 +1,6 @@
+#ifndef CLI_USER_H
+#define CLI_USER_H
+
+void display_menu(); //   Prototype declaration : Procedure Style
+
+#endif // CLI_USER
--- include/cli_utils.h
+++
@@ -1,7 +0,0 @@
-#ifndef CLI_UTILS_H
-#define CLI_UTILS_H
-
-// Cette fonction affichera tes futurs menus
-void afficher_menu_test();
-
-#endif
