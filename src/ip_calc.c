35ef84cd9072ba0d1083d35012bc177e563003af
Lounes BOUTELLIS <lounes.boutellis1@gmail.com>
2026-08-14 14:57:56 +0100

    feat(ip_calc): add the first function to check whether to IP adress is valid or no,

--- src/ip_calc.c
+++ src/ip_calc.c
@@ -1,6 +1,23 @@
-#include <stdio.h>
+#include<stdio.h>
 #include "../include/ip_calc.h"
 
-void tester_moteur_ip() {
-    printf("Moteur de calcul IP prêt à travailler !\n");
+int is_ip_adress_valid(const char *IP){
+    int a,b,c,d;
+    char extra; // Used to catch invalid trailing characters
+
+    // Set up format specifiers to look for integers separated by dots
+    int count = sscanf(IP, "%d.%d.%d.%d%c", &a, &b, &c, &d, &extra);
+
+    // Ensure exactly 4 integers were successfully read
+    if(count == 4){
+        // Check whether the fourth parts of IP adress are between the right bounds (between 0 and 255)
+        if ( ( a>=0 && a<=255 ) && ( b>=0 && b<=255 )
+            && ( c>=0 && c<=255 ) && ( d>=0 && d<=255 )){
+
+            return 1;
+
+        }
+    }
+    return 0;
+    
 }
